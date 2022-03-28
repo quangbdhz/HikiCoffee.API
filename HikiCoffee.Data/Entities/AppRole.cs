77da9bc8894c1b5402a0796ab1d6bdf110c5d353
Tran Quang <tranquangbdhz@gmail.com>
2022-03-28 11:36:43 +0700

    update

---
+++ HikiCoffee.Data/Entities/AppRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HikiCoffee.Data.Entities
+{
+    internal class AppRole : IdentityRole<Guid>
+    {
+        public string Description { get; set; }
+    }
+}
